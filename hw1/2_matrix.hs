ef51b7328004727f5de12e817f5069ab16605162
Caitlyn Lewis <cookcai@oregonstate.edu>
2024-01-15 14:49:35 -0800

    2.a zipWith, 2.b Matrix addition

--- hw1/2_matrix.hs
+++ hw1/2_matrix.hs
@@ -2,20 +2,39 @@ type Row    = [Int]
 type Column = [Int]
 type Matrix = [Row]
 
-
+-- 2.a Vector Addition
 vAdd :: Row -> Row -> Row
 vAdd [] [] = []
 vAdd (x:xs) (y:ys) = x + y : vAdd xs ys
 
+vAddZipWith :: Row -> Row -> Row
+vAddZipWith x y = zipWith (+) x y
+
+-- 2.b Matrix Addition
+mAdd :: Matrix -> Matrix -> Matrix
+mAdd [] [] = []
+mAdd x y = zipWith vAdd x y
+-- To add two matrices, I need to add each row
+
 main :: IO ()
 main = do
     putStrLn "\n(a) - Vector Addition"
-    -- add a num into the empty bag
     let v1 = [ 1, 2, 3, 4]
     let v2 = [ 2, 4, 6, 8]
-    let added = vAdd v1 v2
+    let vA = vAdd v1 v2
+    let vAZ = vAddZipWith v1 v2
 
-    print("Added together: ", added)
+    print("Recursive Version: ", vA)
+    print("zipWith Version:   ", vAZ)
 
+    putStrLn "\n(b) - Matrix Addition"
+    let m1 = [[1,2,3],
+              [4,5,6],
+              [7,8,9]]
+    let m2 = [[10,20,30],
+              [40,50,60],
+              [70,80,90]]
+    let mA = mAdd m1 m2
+    print("Added together: ", mA)
  
     putStrLn "\nDONE"
